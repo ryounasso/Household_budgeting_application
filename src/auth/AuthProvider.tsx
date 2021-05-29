607ffcea1c7fc9c56f20fd76d72c3543533b5fc4
ryonasso <ryounaoyuki@gmail.com>
2021-05-29 20:19:56 +0900

    fix : 認証周りの型変更

--- src/auth/AuthProvider.tsx
+++ src/auth/AuthProvider.tsx
@@ -5,25 +5,35 @@ type AuthContextProps = {
   user: firebase.User | null;
 };
 
-const AuthContext = React.createContext();
+function createCtx<ContextType>() {
+  const ctx = React.createContext<ContextType | undefined>(undefined);
+  const useCtx = () => {
+    const c = React.useContext(ctx);
+    if (!c) throw new Error("useCtx must be inside a Provider with a value");
+    return c;
+  };
+  return [useCtx, ctx.Provider] as const;
+}
 
-const AuthProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+const AuthContext = React.createContext(null);
 
-  const signup = async (email, password, history) => {
+const AuthProvider = ({ children }: any) => {
+  const [currentUser, setCurrentUser] = useState<any>();
+
+  const signup = async (email: string, password: string, history: any) => {
     try {
       await auth.createUserWithEmailAndPassword(email, password);
-      auth.onAuthStateChanged((user) => setCurrentUser(user));
+      auth.onAuthStateChanged((user: any) => setCurrentUser(user));
       history.push("/");
     } catch (error) {
       alert(error);
     }
   };
 
-  const login = async (email, password, history) => {
+  const login = async (email: string, password: string, history: any) => {
     try {
       await auth.signInWithEmailAndPassword(email, password);
-      auth.onAuthStateChanged((user) => setCurrentUser(user));
+      auth.onAuthStateChanged((user: any) => setCurrentUser(user));
       history.push("/");
     } catch (error) {
       alert(error);
